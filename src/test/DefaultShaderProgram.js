92f404d1b9485059c00f1b19e9babbdbdc097c26
Adrian Rocke <business@adrianrocke.com>
2019-08-06 22:20:33 -0500

    remove color change effect

--- src/test/DefaultShaderProgram.js
+++ src/test/DefaultShaderProgram.js
@@ -1,42 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 function DefaultShaderProgram ({ model, translation, resolution }) {
-  const [color, setColor] = useState('1, 0, 0.5, 1')
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setColor('0.5, 1, 0, 1')
-    }, 4000)
-    return () => clearTimeout(timeout)
-  }, [color])
   return (
-      <program>
-        <shader type="vertex">
-          {`
-            attribute vec4 a_position;
+    <program>
+      <shader type="vertex">
+        {`
+          attribute vec4 a_position;
 
-            uniform vec2 u_resolution;
-            uniform vec2 u_translation;
-    
-            void main() {
-              gl_Position = vec4((a_position.xy + u_translation) / u_resolution * vec2(2, -2), 0, 1);
-            }
-          `}
-        </shader>
-        <shader type="fragment">
-          {`
-            precision mediump float;
-          
-            void main() {
-              gl_FragColor = vec4(${color});
-            }
-          `}
-        </shader>
-        <attribute name="a_position">
-          { model }
-        </attribute>
-        <uniform name="u_resolution" value={resolution} type="2f" />
-        <uniform name="u_translation" value={translation} type="2f" />
-      </program>)
+          uniform vec2 u_resolution;
+          uniform vec2 u_translation;
+  
+          void main() {
+            gl_Position = vec4((a_position.xy + u_translation) / u_resolution * vec2(2, -2), 0, 1);
+          }
+        `}
+      </shader>
+      <shader type="fragment">
+        {`
+          precision mediump float;
+        
+          void main() {
+            gl_FragColor = vec4(1, 0, 0.5, 1);
+          }
+        `}
+      </shader>
+      <attribute name="a_position">
+        { model }
+      </attribute>
+      <uniform name="u_resolution" value={resolution} type="2f" />
+      <uniform name="u_translation" value={translation} type="2f" />
+    </program>
+  )
 }
 
 export default DefaultShaderProgram
